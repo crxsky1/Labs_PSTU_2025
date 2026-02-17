a1a6ece0da5265687a788274d4e99e8741ae5c2f
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 21:02:08 +0500

    Add files via upload

---
+++ sem_2/xan_tower/xan_tower.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+
+// n - количество колец
+// start - столбец начала
+// point - цель куда кладем
+// temp - вспомогательный столб
+void xanoi(int n, int start, int point, int temp) {
+	if (n <= 0) return;
+	xanoi(n - 1, start, temp, point);
+	cout << start << " => " << point << endl;
+	xanoi(n - 1, temp, point, start);
+}
+
+int main() {
+	xanoi(3, 1, 3, 2);
+}
