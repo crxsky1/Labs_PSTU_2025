bb18f222473c640b9b909edda8fa3bfe2e5410da
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 20:57:55 +0500

    Add files via upload

---
+++ sem_2/3.4/3.4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "ru");
+	const int n = 6;
+
+	int arr[n] = { 1, 2, 3, 4, 5, 6 };
+
+	bool sorted = true;
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] < arr[i - 1])
+		{
+			sorted = false;
+			break;
+		}
+	}
+	cout << endl;
+
+	if (sorted)
+	{
+		cout << "Массив отсортирован!" << endl;
+	}
+	else { cout << "Массив не упорядочен по возрастанию" << endl; }
+
+
+	return 0;
+}
