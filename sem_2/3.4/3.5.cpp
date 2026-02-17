09aeacc3ae516dffc5cfaaf23c1e716b9161f828
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 20:56:58 +0500

    Add files via upload

---
+++ sem_2/3.4/3.5.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int M, tmp, max;
+	const int N = 9;
+
+
+	int arr[N]{ 1,3,7,4,9,2,6,8,5 };
+	for (int i = 0; i <= N; i++) {
+		cout << arr[N];
+
+	}
+	
+
+
+}
