43e508ccba433ec6c4e2b3fcbef86d95bb9911ea
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 20:59:55 +0500

    Add files via upload

---
+++ sem_2/fibonacci/fibonacci.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+	if (n <= 1) return 0;
+	else if (n == 2) return 1;
+	return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int main() {
+	for (size_t i = 1; i < 20; i++) {
+		cout << fibonacci(i) << " ";
+	}
+}
