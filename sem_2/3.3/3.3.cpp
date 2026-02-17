eefb26c0359d17febf4fb3ecbb7e55220c53d642
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 20:56:31 +0500

    Add files via upload

---
+++ sem_2/3.3/3.3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+setlocale(LC_ALL, "ru");
+
+	const int n = 11;
+	int max;
+	int count = 1;
+
+	int arr[n] = { 41, 42, 66, 87, 86, 38, 77, 84, 45, 35, 22 };
+
+	max = arr[0];
+	cout << arr[0] << " ";
+	for (int i = 1; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	
+		if (arr[i] > max)
+		{
+			max = arr[i];
+			count = 1;
+		}
+		else if (arr[i] == max) { count++; }
+	}
+	cout << endl;
+
+	cout << "Max: " << max << endl;
+	cout << "Количество элементов с этим значением: " << count << endl;
+
+	return 0;
+}
