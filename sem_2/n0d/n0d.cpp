33283e0a3606d08270f3cd5fc2f9afd0a22f8a82
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 21:03:18 +0500

    Add files via upload

---
+++ sem_2/n0d/n0d.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int nod(int n, int m) {
+	if (n == m) return n;
+	if (n > m) return nod(n - m, m);
+	return nod(n, m - n);
+}
+
+int main() {
+	cout << " nod(10, 3)" << "\t" << nod(10, 3) << endl;
+	cout << " nod(40, 12)" << "\t" << nod(40, 12) << endl;
+	cout << " nod(13, 13)" << "\t" << nod(13, 13) << endl;
+	cout << " nod(5, 25)" << "\t" << nod(5, 25) << endl;
+}
