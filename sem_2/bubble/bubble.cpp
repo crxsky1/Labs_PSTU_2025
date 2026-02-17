f7b4c3a4645cd8a30ebb6d53943ace44a079168d
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 20:58:22 +0500

    Add files via upload

---
+++ sem_2/bubble/bubble.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	const int N = 5;
+	int arr[N] = { 3,2,4,1,5 };
+	int tmp;
+
+	for (int k = 0; k < N - 1; k++) {
+		for (int i = 0; i < N - 1; i++) {
+			if (arr[i] < arr[i + 1]) {
+
+			}
+			else {
+				tmp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = tmp;
+
+			}
+			for (int j = 0; j < N; j++) {
+				cout << arr[j];
+			}
+			cout << endl;
+		}
+	}
+
+
+
+
+}
