0f1ad2ab2262680936272072f1c7ced3e7924063
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-17 20:56:07 +0500

    Add files via upload

---
+++ sem_2/3.2/3.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 11;
+	int tmp, p, q;
+
+	int arr[n] = { 41, 42, 66, 87, 86, 38, 77, 84, 45, 35, 22 };
+
+	p = 2; q = 6;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	while (p < q)
+	{
+		tmp = arr[p];
+		arr[p] = arr[q];
+		arr[q] = tmp;
+		p++;
+		q--;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
