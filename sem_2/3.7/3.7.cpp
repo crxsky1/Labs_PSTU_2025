b2de3d25ee2d2fab159ad35826007ac445cc1d15
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-18 10:59:08 +0500

    Add files via upload

---
+++ sem_2/3.7/3.7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+void show(int arr[100], int N) {
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	srand(time(0));
+	int N = 10;
+	int arr[100];
+	for (int i = 0; i < N; i++) {
+		arr[i] = ((float)rand() / RAND_MAX) * (20 - 5) + 5;
+	}
+	show(arr, N);
+	int max = arr[0];
+	int min = arr[0];
+	for (int i = 1; i < N; i++) {
+		if (arr[i] > max) max = arr[i];
+		if (arr[i] < min) min = arr[i];
+	}
+	cout << endl << max << " " << min;
+}
