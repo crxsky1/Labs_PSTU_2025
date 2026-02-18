094abb9412799159dc24cb74fe522ae223422c32
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-18 10:59:52 +0500

    Add files via upload

---
+++ sem_2/3.9/3.9.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+void show(int arr[100], int N) {
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	srand(time(0));
+	int N = 5;
+	int arr[100];
+	for (int i = 0; i < N; i++) {
+		arr[i] = ((float)rand() / RAND_MAX) * 20;
+	}
+	show(arr, N);
+	for (int i = 0; i < N; i++) {
+		if (arr[i] % 2 != 0) {
+			for (int j = i; j < N; j++) {
+				arr[j] = arr[j + 1];
+			}
+			N--;
+		}
+	}
+	show(arr, N);
+}
