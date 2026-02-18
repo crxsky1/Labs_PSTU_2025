74dcc93a75e80fc2ffab14ade4b0e064a7132b0b
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-18 11:01:15 +0500

    Add files via upload

---
+++ sem_2/3.13/3.13.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+void show(int arr[4][4], int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+int main() {
+	srand(time(0));
+	const int N = 4;
+	int arr[N][N];
+	int s = 0;
+	int tmp;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j] = ((float)rand() / RAND_MAX) * (20 - 1) + 1;
+		}
+	}
+	show(arr, N);
+	for (int j = 0; j < N - 1; j++) {
+		for (int i = 0; i < N - j - 1; i++) {
+			if (arr[s][i] > arr[s][i + 1]) {
+				for (int k = 0; k < N; k++) {
+					tmp = arr[k][i];
+					arr[k][i] = arr[k][i + 1];
+					arr[k][i + 1] = tmp;
+				}
+			}
+		}
+	}
+	show(arr, N);
+}
