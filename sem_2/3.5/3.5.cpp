3e24a8e37e11869d43606b9cd71f15b9f12665b1
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-02-22 18:52:03 +0500

    3.5.cpp

--- sem_2/3.5/3.5.cpp
+++ sem_2/3.5/3.5.cpp
@@ -1,16 +1,29 @@
-﻿#include<iostream>
+#include <iostream>
+
 using namespace std;
-int main() {
-	int M, tmp, max;
-	const int N = 9;
 
-
-	int arr[N]{ 1,3,7,4,9,2,6,8,5 };
-	for (int i = 0; i <= N; i++) {
-		cout << arr[N];
-
+void show(int arr[100], int N) {
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << " ";
 	}
+	cout << endl;
+}
+
+int main() {
+	srand(time(0));
+
+	int N = 10;
+	int arr[100];
+
+
+	for (int i = 0; i < N; i++) {
+		arr[i] = ((float)rand() / RAND_MAX) * 20;
+	}
+	show(arr, N);
 	
-
-
+	int mx = arr[0];
+	for (int i = 1; i < N; i++) {
+		if (arr[i] > mx) mx = arr[i];
+	}
+	cout << endl << mx;
 }
