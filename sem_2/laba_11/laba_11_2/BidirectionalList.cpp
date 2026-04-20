d541c00aaa70ab9717f67fe586addcdad4e1430a
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-04-20 14:22:24 +0500

    Add files via upload

---
+++ sem_2/laba_11/laba_11_2/BidirectionalList.cpp
@@ -0,0 +1,328 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void invertSort(int* arr, int size) {
+	int tmp;
+	for (int j = 0; j < size - 1; j++) {
+		for (int i = 0; i < size - j - 1; i++) {
+			if (arr[i] < arr[i + 1]) {
+				tmp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = tmp;
+			}
+		}
+	}
+}
+
+// двунаправленый список
+struct BidirectionalList {
+private:
+	struct Node {
+		string data;
+		Node* nextElementAddress = nullptr;
+		Node* prevElementAddress = nullptr;
+	};
+
+public:
+	int size = 0;
+	Node* firstElementAddress = nullptr;
+	Node* endElementAddress = nullptr;
+
+	void print() {
+		Node* node = firstElementAddress;
+		if (size == 0) {
+			cout << " список пустой" << endl;
+		}
+		else {
+			for (int i = 0; i < size; i++) {
+				cout << " " << i << ": \t" << node->data /* << "    \t" << node->prevElementAddress << "\t" << node << "\t" << node->nextElementAddress*/ << endl;
+				node = node->nextElementAddress;
+			}
+		}
+		cout << endl;
+	}
+	string getData(int node_i) {
+		if (node_i < size) {
+			Node* node = firstElementAddress;
+			for (int i = 0; i < node_i; i++) {
+				node = node->nextElementAddress;
+			}
+			return node->data;
+		}
+		else {
+			cout << "элемента с номером " << node_i << " не существует, так как " << node_i << " >= " << size;
+			return "";
+		}
+	}
+
+	BidirectionalList() {};
+	BidirectionalList(int k, ...) {
+		// тоже самое что и addKElements()
+
+		va_list args;
+		va_start(args, k);
+
+		const char* data; // string не работает
+		int index;
+
+		for (int i = 0; i < k; i++) {
+			data = va_arg(args, const char*);
+			index = va_arg(args, int);
+
+			add(data, index);
+		}
+
+		va_end(args);
+	}
+
+	/////////////////// add ///////////////////
+	void add(string data, int node_i) {
+		if (node_i > size) {
+			cout << "номер больше размера списка, элемент будет добавлен в конец " << node_i << " > " << size << endl << endl;
+			node_i = size;
+		}
+
+		Node* node;
+		if (node_i < size / 2) {
+			node = firstElementAddress;
+			for (int i = 0; i < node_i - 1; i++) {
+				node = node->nextElementAddress;
+			}
+		}
+		else {
+			node = endElementAddress;
+			for (int i = size - 1; i > node_i - 1; i--) {
+				node = node->prevElementAddress;
+			}
+		}
+
+
+
+		if (firstElementAddress == nullptr) {
+			firstElementAddress = new Node{ data, nullptr, nullptr };
+			endElementAddress = firstElementAddress;
+
+		}
+		else if (node_i == 0) {
+			Node* newNode = new Node{ data, firstElementAddress, nullptr };
+			firstElementAddress->prevElementAddress = newNode;
+		}
+		else if (node_i == size) {
+			Node* newNode = new Node{ data, nullptr, node };
+			node->nextElementAddress = newNode;
+
+			endElementAddress = newNode;
+		}
+		else {
+			Node* newNode = new Node{ data, node->nextElementAddress, node };
+
+			node->nextElementAddress->prevElementAddress = newNode;
+			node->nextElementAddress = newNode;
+		}
+		size++;
+
+
+	}
+	void addInEnd(string data) {
+		add(data, size);
+	}
+
+	void addKElements(int k, ...) {
+		// addKElements(int k, "data1", index1, "data2", index2, ...)
+		//					   |____k = 1____|   |____k = 2____|	
+
+		va_list args;
+		va_start(args, k);
+
+		const char* data; // string не работает
+		int index;
+
+		for (int i = 0; i < k; i++) {
+			data = va_arg(args, const char*);
+			index = va_arg(args, int);
+
+			add(data, index);
+		}
+
+		va_end(args);
+	}
+	///////////////////////////////////////////
+
+
+	/////////////////// del ///////////////////
+	void del(int node_i) {
+		if (node_i >= size) {
+			cout << "номер больше размера списка, будет удален последний элемент " << node_i << " > " << size << endl << endl;
+			node_i = size - 1;
+		}
+
+		Node* node;
+		if (node_i < size / 2) {
+			node = firstElementAddress;
+			for (int i = 0; i < node_i; i++) {
+				node = node->nextElementAddress;
+			}
+		}
+		else {
+			node = endElementAddress;
+			for (int i = size - 1; i > node_i; i--) {
+				node = node->prevElementAddress;
+			}
+		}
+
+
+		if (node_i == 0) {
+			firstElementAddress = node->nextElementAddress;
+			if (firstElementAddress != nullptr) firstElementAddress->prevElementAddress = nullptr;
+			delete node;
+		}
+		else if (node_i == size) {
+			node->prevElementAddress->nextElementAddress = nullptr;
+			endElementAddress = node->prevElementAddress;
+
+			delete node;
+		}
+		else {
+			node->nextElementAddress->prevElementAddress = node->prevElementAddress;
+			node->prevElementAddress->nextElementAddress = node->nextElementAddress;
+
+			delete node;
+		}
+
+		size--;
+	}
+	void delEndElement() {
+		del(size);
+	}
+
+	void delKElements(int k, ...) {
+		va_list args;
+		va_start(args, k);
+
+		int* arr = new int[k];
+
+
+		for (int i = 0; i < k; i++) {
+			arr[i] = va_arg(args, int);
+		}
+		invertSort(arr, k); // нужно удалять начиная с наибольшего
+		// индекса, чтобы сохранить индексацию
+
+
+		for (int i = 0; i < k; i++) {
+			del(arr[i]);
+		}
+
+		delete[] arr;
+		va_end(args);
+	}
+	//////////////////////////////////////////
+
+	////////////////// file //////////////////
+	void saveInFile(string name) {
+		ofstream file(name);
+
+		Node* node = firstElementAddress;
+		for (int i = 0; i < size; i++) {
+			file << node->data << "\n";
+			node = node->nextElementAddress;
+		}
+		cout << " список сохранен в файл: " << name << endl;
+		file.close();
+	}
+
+	void loadFromFileAddInEnd(string name) {
+		ifstream file(name);
+		string str = " ";
+		while (getline(file, str)) {
+			addInEnd(str);
+		}
+		file.close();
+		cout << " список загружен из файла: " << name << endl;
+	}
+	void loadFromFileOverwriting(string name) {
+		deleteList();
+		loadFromFileAddInEnd(name);
+	}
+	/////////////////////////////////////////
+
+	void deleteList() {
+		for (int i = 0; i < size; i++) {
+			del(0);
+		}
+		size = 0;
+		cout << " список удален" << endl;
+	}
+	~BidirectionalList() {
+		if (size > 0) deleteList();
+	}
+};
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	setlocale(LC_ALL, "ru");
+
+
+	////////////////// 	1  //////////////////
+	BidirectionalList list(5,
+		"data0", 0,
+		"data1", 1,
+		"data2", 2,
+		"data3", 3,
+		"data4", 4
+	);
+
+
+
+	cout << "\n ------------------  2  ------------------" << endl;
+
+	BidirectionalList emptyList;
+	cout << "  emptyList:" << endl;
+	emptyList.print();
+
+	cout << "     list:" << endl;
+	list.print();
+
+	emptyList.deleteList(); // лист удаляется автоматически, и без этого
+
+
+	cout << " ------------------  3  ------------------" << endl;
+	cout << " добавление элементов на позиции 2, 6, 4" << endl;
+	list.addKElements(3,
+		"newData1", 2,
+		"newData2", 6,
+		"newData3", 4
+	);
+	list.print();
+
+
+	cout << " удаление элементов на позициях 3, 1, 5" << endl;
+	list.delKElements(3, 3, 1, 5);
+	list.print();
+
+
+	cout << " ------------------  7  ------------------" << endl;
+	list.saveInFile("myList.txt");
+	cout << "     list:" << endl;
+	list.deleteList();
+	list.print();
+
+
+	cout << " ------------------  9  ------------------" << endl;
+	list.loadFromFileOverwriting("myList.txt");
+	cout << "     list:" << endl;
+	list.print();
+
+
+	cout << " ------------------ 1 0 ------------------" << endl;
+	list.deleteList();
+
+
+}
