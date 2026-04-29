c8fbf76ac4741db2af74c8ce1811e04eb25ab3c5
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-04-29 10:08:19 +0500

    Add files via upload

---
+++ sem_2/count_sort/count_sort.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+void countingSort(int arr[], int n) {
+	if (n <= 1) return;
+	int maxVal = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (arr[i] > maxVal) maxVal = arr[i];
+	}
+	int* count = new int[maxVal + 1]();
+	for (int i = 0; i < n; i++) {
+		count[arr[i]]++;
+	}
+	for (int i = 1; i <= maxVal; i++) {
+		count[i] += count[i - 1];
+	}
+	int* output = new int[n];
+	for (int i = n - 1; i >= 0; i--) {
+		output[count[arr[i]] - 1] = arr[i];
+		count[arr[i]]--;
+	}
+	for (int i = 0; i < n; i++) {
+		arr[i] = output[i];
+	}
+	delete[] count;
+	delete[] output;
+}
+void printArray(int arr[], int n) {
+	cout << "[";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i];
+		if (i < n - 1) cout << ", ";
+	}
+	cout << "]" << endl;
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	int arr[] = { 2, 5, 3, 0, 2, 3, 2, 0, 3 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Исходный массив: ";
+	printArray(arr, n);
+	countingSort(arr, n);
+	cout << "Отсортированный массив: ";
+	printArray(arr, n);
+	return 0;
+}
