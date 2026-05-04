75af45ff66714035cefec9415bf195356f7bee32
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-05-04 13:39:59 +0500

    Add files via upload

--- sem_2/class/class_6/class_6.cpp
+++ sem_2/class/class_6/class_6.cpp
@@ -1,1 +1,14 @@
+﻿#include <iostream>
+#include <string>
 
+using namespace std;
+
+#include "list.h"
+
+int main() {
+	List<int> listI = { 1, 2, 3, 4 };
+
+	for (auto it = listI.rbegin(); it != listI.rend(); it++) {
+		cout << *it << " ";
+	}
+}
