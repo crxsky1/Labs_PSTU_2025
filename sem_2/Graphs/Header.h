8499fb07cba8ef7ac5f0d30bdc7983669fa199c1
Daniel <72625835+crxsky1@users.noreply.github.com>
2026-05-27 00:31:40 +0500

    Add files via upload

---
+++ sem_2/Graphs/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>		
+#include<list>
+#include<queue>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+#include"Time.h"
+#include"number1.h"
+#include"number2.h"
+#include"number3.h"
